64a584a3ea065f0352cb1abf4ead16ef274aea9e
samuelhaile <samuel.haileemail@miu.edu>
2024-09-20 21:08:26 -0500

    mini bank app project

---
+++ model/bankingArray.js
@@ -0,0 +1,13 @@
+
+
+function bankingArray(AccountNumber , fullName , TypeOfAccount){
+
+        this.AccountNumber = AccountNumber ; 
+        this.fullName =fullName ;
+        this.TypeOfAccount = TypeOfAccount ;
+
+
+}
+
+
+module.exports = bankingArray ; 
